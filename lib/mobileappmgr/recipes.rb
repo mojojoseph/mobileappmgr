7d63683bc329b75939c3c7ceeaf4bd250370406c
Joseph Bell <jbell@numerex.com>
2012-04-05 14:34:16 -0500

    Handle Android

--- lib/mobileappmgr/recipes.rb
+++ lib/mobileappmgr/recipes.rb
@@ -17,12 +17,12 @@ Capistrano::Configuration.instance.load do
         ios_build_dir      = Pathname.getwd.join build_config['android']['directory']
 
         apks = Dir["#{apk_build_dir}/*.apk"]
-
-        ipas.each do |ipa|
-
-          fname = File.basename(ipa)
-          puts "IPA = #{fname}"
-          upload(ipa.to_s, "#{shared_path}/mobileapps/#{fname}")
+        
+        apks.each do |apk|
+          
+          fname = File.basename(apk)
+          puts "APK = #{fname}"
+          upload(apk.to_s, "#{shared_path}/mobileapps/#{fname}")
           
           apptarget = build_config['ios']['apptarget']
           fname =~ /#{apptarget}\.(.*)\.apk/ # Get the version
@@ -40,41 +40,35 @@ name:  #{build_config['application']['name']}
 version:  #{version}
 YML_DOCUMENT
 
-        yml_fname = "#{build_config['application']['name']}_#{version}.yml"
-        File.open(yml_fname, 'w') {|f| f.write(yml_content)}
-        upload(yml_fname, "#{shared_path}/mobileapps/#{yml_fname}")
+          yml_fname = "#{build_config['application']['name']}_#{version}.yml"
+          File.open(yml_fname, 'w') {|f| f.write(yml_content)}
+          upload(yml_fname, "#{shared_path}/mobileapps/#{yml_fname}")
+        end
       end
 
-
-
-
+      desc "Publish an IPA to a remote deployment server"
+      task :ipa do
+        puts "Uploading IPA to #{current_release}"
         
-      end
-    end
-
-    desc "Publish an IPA to a remote deployment server"
-    task :ipa do
-      puts "Uploading IPA to #{current_release}"
-
-      configuration_file = Pathname.getwd.join 'mobileapps.yml'
-      build_config       = YAML::load(File.open(configuration_file))
-      ios_build_dir      = Pathname.getwd.join build_config['ios']['directory']
-
-      ipas = Dir["#{ios_build_dir}/*.ipa"]
-
-      ipas.each do |ipa|
-
-        fname = File.basename(ipa)
-        puts "IPA = #{fname}"
-        upload(ipa.to_s, "#{shared_path}/mobileapps/#{fname}")
-
-        apptarget = build_config['ios']['apptarget']
-        fname =~ /#{apptarget}\.(.*)\.ipa/ # Get the version
-        version = $1                       # Perl, save us
-        uploaded = Time.now.strftime("%Y-%m-%d %H:%M:%S %Z").to_yaml
-        whoami   = `whoami`.chop
-
-        yml_content = <<-YML_DOCUMENT
+        configuration_file = Pathname.getwd.join 'mobileapps.yml'
+        build_config       = YAML::load(File.open(configuration_file))
+        ios_build_dir      = Pathname.getwd.join build_config['ios']['directory']
+        
+        ipas = Dir["#{ios_build_dir}/*.ipa"]
+        
+        ipas.each do |ipa|
+          
+          fname = File.basename(ipa)
+          puts "IPA = #{fname}"
+          upload(ipa.to_s, "#{shared_path}/mobileapps/#{fname}")
+          
+          apptarget = build_config['ios']['apptarget']
+          fname =~ /#{apptarget}\.(.*)\.ipa/ # Get the version
+          version = $1                       # Perl, save us
+          uploaded = Time.now.strftime("%Y-%m-%d %H:%M:%S %Z").to_yaml
+          whoami   = `whoami`.chop
+          
+          yml_content = <<-YML_DOCUMENT
 platform:  ios
 uploaded:  #{uploaded}
 creator:  #{whoami}
@@ -84,11 +78,11 @@ name:  #{build_config['application']['name']}
 version:  #{version}
 YML_DOCUMENT
 
-        yml_fname = "#{build_config['application']['name']}_#{version}.yml"
-        File.open(yml_fname, 'w') {|f| f.write(yml_content)}
-        upload(yml_fname, "#{shared_path}/mobileapps/#{yml_fname}")
+          yml_fname = "#{build_config['application']['name']}_#{version}.yml"
+          File.open(yml_fname, 'w') {|f| f.write(yml_content)}
+          upload(yml_fname, "#{shared_path}/mobileapps/#{yml_fname}")
+        end
       end
     end
   end
-end
 end
