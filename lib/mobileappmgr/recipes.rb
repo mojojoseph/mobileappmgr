8abd1137b25dfc89c201756504f216cc8993ca41
Joseph Bell <jbell@numerex.com>
2012-04-05 15:00:59 -0500

    Refactoring

--- lib/mobileappmgr/recipes.rb
+++ lib/mobileappmgr/recipes.rb
@@ -60,42 +60,14 @@ YML_DOCUMENT
         workhorse "apk"
       end
 
-      desc "Publish an IPA to a remote deployment server"
+      desc "Publish an iOS IPA to a remote deployment server"
       task :ipa do
-        puts "Uploading IPA to #{current_release}"
-        
-        configuration_file = Pathname.getwd.join 'mobileapps.yml'
-        build_config       = YAML::load(File.open(configuration_file))
-        ios_build_dir      = Pathname.getwd.join build_config['application']['directory']
-        
-        ipas = Dir["#{ios_build_dir}/*.ipa"]
-        
-        ipas.each do |ipa|
-          
-          fname = File.basename(ipa)
-          puts "IPA = #{fname}"
-          upload(ipa.to_s, "#{shared_path}/mobileapps/#{fname}")
-          
-          apptarget = build_config['application']['apptarget']
-          fname =~ /#{apptarget}\.(.*)\.ipa/ # Get the version
-          version = $1                       # Perl, save us
-          uploaded = Time.now.strftime("%Y-%m-%d %H:%M:%S %Z").to_yaml
-          whoami   = `whoami`.chop
-          
-          yml_content = <<-YML_DOCUMENT
-platform:  ios
-uploaded:  #{uploaded}
-creator:  #{whoami}
-apptarget:  #{build_config['application']['apptarget']}
-bundleid:  #{build_config['ios']['bundleid']}
-name:  #{build_config['application']['name']}
-version:  #{version}
-YML_DOCUMENT
+        workhorse "ipa"
+      end
 
-          yml_fname = "#{build_config['application']['name']}_ipa_#{version}.yml"
-          File.open(yml_fname, 'w') {|f| f.write(yml_content)}
-          upload(yml_fname, "#{shared_path}/mobileapps/#{yml_fname}")
-        end
+      task :all do
+        mobileappmgr.apk
+        mobileappmgr.ipa
       end
     end
   end
