8ba1ef7eb0b95e6865a3946022127f4711a1523f
Joseph Bell <jbell@numerex.com>
2012-04-05 14:53:41 -0500

    Refactoring

--- lib/mobileappmgr/recipes.rb
+++ lib/mobileappmgr/recipes.rb
@@ -2,8 +2,45 @@ Capistrano::Configuration.instance.load do
 
   namespace :mobileappmgr do
 
-    def workhorse
-      puts "WORKHORSE"
+    # type needs be 'ipa' or 'apk'
+    def workhorse(type)
+
+      configuration_file = Pathname.getwd.join 'mobileapps.yml'
+      build_config       = YAML::load(File.open(configuration_file))
+      app_build_dir      = Pathname.getwd.join build_config['application']['directory']
+      
+      apps = Dir["#{app_build_dir}/*.#{type}"]
+
+      apps.each do |app|
+          
+        fname = File.basename(app)
+        puts "APP = #{fname}"
+        upload(app.to_s, "#{shared_path}/mobileapps/#{fname}")
+          
+        apptarget = build_config['application']['apptarget']
+        fname =~ /#{apptarget}\.(.*)\.#{type}/ # Get the version
+        version = $1                       # Perl, save us
+        uploaded = Time.now.strftime("%Y-%m-%d %H:%M:%S %Z").to_yaml
+        whoami   = `whoami`.chop
+
+        type == "apk" ? platform = "android" : platform = "iOS"
+          
+        yml_content = <<-YML_DOCUMENT
+platform:  #{platform}
+uploaded:  #{uploaded}
+creator:  #{whoami}
+apptarget:  #{build_config['android']['apptarget']}
+bundleid:  #{build_config['android']['bundleid']}
+name:  #{build_config['application']['name']}
+version:  #{version}
+YML_DOCUMENT
+
+        yml_fname = "#{build_config['application']['name']}_#{type}_#{version}.yml"
+        File.open(yml_fname, 'w') {|f| f.write(yml_content)}
+        upload(yml_fname, "#{shared_path}/mobileapps/#{yml_fname}")
+      end
+      
+
     end
 
     desc "Symlink"
@@ -15,41 +52,7 @@ Capistrano::Configuration.instance.load do
     desc "Publish an Android APK to a remote deployment server"
     namespace :publish do
       task :apk do
-
-        workhorse
-
-        configuration_file = Pathname.getwd.join 'mobileapps.yml'
-        build_config       = YAML::load(File.open(configuration_file))
-        apk_build_dir      = Pathname.getwd.join build_config['application']['directory']
-
-        apks = Dir["#{apk_build_dir}/*.apk"]
-        
-        apks.each do |apk|
-          
-          fname = File.basename(apk)
-          puts "APK = #{fname}"
-          upload(apk.to_s, "#{shared_path}/mobileapps/#{fname}")
-          
-          apptarget = build_config['application']['apptarget']
-          fname =~ /#{apptarget}\.(.*)\.apk/ # Get the version
-          version = $1                       # Perl, save us
-          uploaded = Time.now.strftime("%Y-%m-%d %H:%M:%S %Z").to_yaml
-          whoami   = `whoami`.chop
-          
-          yml_content = <<-YML_DOCUMENT
-platform:  android
-uploaded:  #{uploaded}
-creator:  #{whoami}
-apptarget:  #{build_config['android']['apptarget']}
-bundleid:  #{build_config['android']['bundleid']}
-name:  #{build_config['application']['name']}
-version:  #{version}
-YML_DOCUMENT
-
-          yml_fname = "#{build_config['application']['name']}_apk_#{version}.yml"
-          File.open(yml_fname, 'w') {|f| f.write(yml_content)}
-          upload(yml_fname, "#{shared_path}/mobileapps/#{yml_fname}")
-        end
+        workhorse "apk"
       end
 
       desc "Publish an IPA to a remote deployment server"
